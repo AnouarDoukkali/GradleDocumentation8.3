b7491a4f23d4501941b33603d82d7401b2926681
Anouar Doukkali <Anouar.doukkali@gmail.com>
2023-09-13 21:17:17 +0100

    buildscript Block

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
     `kotlin-dsl`
 }
+
+
+// when the buildScript need external dependencies , those can be declared under buildScript Block
+// In multi-project builds, dependencies declared using a project's buildscript() method are
+// accessible to the build scripts of all its sub-projects.
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+    }
+}
