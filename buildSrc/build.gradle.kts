6ebf205563605c7921669511796be9ce5d143ed2
Anouar Doukkali <Anouar.doukkali@gmail.com>
2023-09-14 09:43:27 +0100

    convention custom plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    `java-gradle-plugin` // gradle development plugin
 }
 println("This is executed during the configuration phase in buildSrc build.gradle.kts")
 extra["printName"] = true
@@ -16,3 +17,12 @@ buildscript {
     }
 }
 
+// defining the plugins being built 
+gradlePlugin {
+    plugins {
+        create("myPlugins") {
+            id = "my-plugin"
+            implementationClass = "convention.plugins.MyPlugin"
+        }
+    }
+}
